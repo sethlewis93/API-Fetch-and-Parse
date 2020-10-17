d726d18fca5b853ade284f2c703290bbe9410e11
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-10-17 16:14:52 -0400

    appended search bar to DOM

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+const appendToDiv = (elementName, parentSelector) => {
+    const element = document.createElement(elementName)
+    const parent = document.querySelector(parentSelector)
+    parent.appendChild(element);
+    return element;
+};
+
+
+const search = appendToDiv('form', '.search-container');
+search.insertAdjacentHTML('afterbegin', '<input type="search" id="search-input" class="search-input" placeholder="Search..."><input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
