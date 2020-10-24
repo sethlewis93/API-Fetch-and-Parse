035bfa5092899868ac95ecae09c773ef420a1a33
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-10-24 15:55:35 -0400

    Revert "svae for later"
    This reverts commit d02d58b5a3440043865a4f85bbefd81432be6dfc.

--- js/drafts.js
+++ js/drafts.js
@@ -6,16 +6,27 @@
 
 
 
-const searchFeature = (data) => {
-  const cards = document.querySelectorAll('.card');
-  const searchfield = document.querySelector('#search-input');
-  const userSearch = searchfield.value.toLowerCase(); 
+// SEARCH 
+const searchFeature2 = () => {
+    const cards = document.querySelectorAll('.card');
+    let searchInput = document.querySelector('#search-input')
+    let matches = [];
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.display = "none";
+        let employeeName = document.querySelectorAll('#name');
+        if (searchInput.value.length !== 0) {
+            console.log(searchInput.value); 
+          if (employeeName.textContent.includes(searchInput.value)) {
+            matches.push(cards[i]);
+          }
+        }
+      }
+      if (matches.length === 0) {
+      gallery.insertAdjacentHTML('afterbegin', `<h3>Sorry - your search yeilded no matches`);
+      } else {
+        gallery.insertAdjacentHTML('afterbegin', `${matches}`);
+      }
+}
 
-  data.filter(employee => {
-      const firstName = employee.name.first;
-      const lastName = employee.name.last;
-      const fullName = `${firstName} ${lastName}`
-  });
-  return data;
-};    
+search.addEventListener('input', searchFeature2);
 
