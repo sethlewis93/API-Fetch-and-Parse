d02d58b5a3440043865a4f85bbefd81432be6dfc
sethlewis93 <64318680+sethlewis93@users.noreply.github.com>
2020-10-24 15:53:21 -0400

    svae for later

--- js/drafts.js
+++ js/drafts.js
@@ -6,27 +6,16 @@
 
 
 
-// SEARCH 
-const searchFeature2 = () => {
-    const cards = document.querySelectorAll('.card');
-    let searchInput = document.querySelector('#search-input')
-    let matches = [];
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].style.display = "none";
-        let employeeName = document.querySelectorAll('#name');
-        if (searchInput.value.length !== 0) {
-            console.log(searchInput.value); 
-          if (employeeName.textContent.includes(searchInput.value)) {
-            matches.push(cards[i]);
-          }
-        }
-      }
-      if (matches.length === 0) {
-      gallery.insertAdjacentHTML('afterbegin', `<h3>Sorry - your search yeilded no matches`);
-      } else {
-        gallery.insertAdjacentHTML('afterbegin', `${matches}`);
-      }
-}
+const searchFeature = (data) => {
+  const cards = document.querySelectorAll('.card');
+  const searchfield = document.querySelector('#search-input');
+  const userSearch = searchfield.value.toLowerCase(); 
 
-search.addEventListener('input', searchFeature2);
+  data.filter(employee => {
+      const firstName = employee.name.first;
+      const lastName = employee.name.last;
+      const fullName = `${firstName} ${lastName}`
+  });
+  return data;
+};    
 
